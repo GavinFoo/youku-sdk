7d24f466c58cd7e7c8bf9c3284939febf4516bb7
harris <harris@5253w.com>
2016-05-10 17:15:16 +0800

    add youku sdk

---
+++ YouTuEngineMediaPlayer/YouTuMediaPlayerEngineYoukuHeaders/YTEngineOpenViewPlayer.h
@@ -0,0 +1,18 @@
+//
+//  YTEngineOpenViewPlayer.h
+//  YouTuEngineMediaPlayer
+//
+//  Created by 周娜 on 15/10/12.
+//  Copyright (c) 2015年 Youku Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "YoukuMediaPlayer.h"
+
+@class YTEngineReachability;
+
+@interface YTEngineOpenViewPlayer : UIViewController
+
+@property (nonatomic, retain) YYMediaPlayer *player;
+
+@end
